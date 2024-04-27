ac4f78cbf96e2003aea9201c849fb72aeaf31d3d
Muhammed ALDRUBI <MuhammedALDRUBI>
2024-04-27 18:05:08 +0300

    testing

--- src/DataResources/DBFetcherDataResources/Traits/StatisticsProcessingMethods.php
+++ src/DataResources/DBFetcherDataResources/Traits/StatisticsProcessingMethods.php
@@ -261,7 +261,6 @@ trait StatisticsProcessingMethods
              * Result Data Processing Part
              */
 
-            $data = $this->processData($this->query->dd());
             $data = $this->processData($this->query->get()->toArray());
             $this->mergeProcessedData($data);
         }
