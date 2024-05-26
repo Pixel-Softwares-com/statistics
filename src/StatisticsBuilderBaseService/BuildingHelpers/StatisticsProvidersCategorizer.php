ddc7a58db4ecd74c40007a94d58775e48211c667
Muhammed ALDRUBI <MuhammedALDRUBI>
2024-05-26 10:26:20 +0300

    fixing a bug

--- src/StatisticsBuilderBaseService/BuildingHelpers/StatisticsProvidersCategorizer.php
+++ src/StatisticsBuilderBaseService/BuildingHelpers/StatisticsProvidersCategorizer.php
@@ -11,6 +11,7 @@ class StatisticsProvidersCategorizer implements Arrayable
 {
     protected array $statisticsProvidersClass = [];
 
+    protected array $normalPrioritedStatisticsProviders = [];
     protected array $ReformatorStatisticsProviders = [];
     protected array $ReforulatableStatisticsProviders = [];
     public function __construct(array $statisticsProvidersClass = [])
@@ -33,6 +34,14 @@ class StatisticsProvidersCategorizer implements Arrayable
     {
         return $this->ReforulatableStatisticsProviders;
     }
+
+    /**
+     * @return array
+     */
+    public function getNormalPrioritedStatisticsProviders(): array
+    {
+        return $this->normalPrioritedStatisticsProviders;
+    }
     /**
      * @param array $statisticsProvidersClass
      * @return $this
@@ -42,29 +51,48 @@ class StatisticsProvidersCategorizer implements Arrayable
         $this->statisticsProvidersClass = $statisticsProvidersClass;
         return $this;
     }
+    protected function orderNormalPrioritedStatisticsProvider(StatisticsProviderDecorator $statisticsProvider) : void
+    {
+        $key = get_class($statisticsProvider);
+        $this->normalPrioritedStatisticsProviders[ $key ] = $statisticsProvider;
+    }
     public static function DoesItHaveReforulatableData(StatisticsProviderDecorator $statisticsProvider ) : bool
     {
         return $statisticsProvider instanceof HasReformulatableData;
     }
-    protected function orderReformulatableSatisticsProvider(StatisticsProviderDecorator $statisticsProvider) : void
+    protected function orderReformulatableSatisticsProvider(StatisticsProviderDecorator $statisticsProvider) : bool
     {
         if($this::DoesItHaveReforulatableData($statisticsProvider))
         {
             $key = get_class($statisticsProvider);
             $this->ReforulatableStatisticsProviders[ $key ] = $statisticsProvider;
+            return true;
         }
+        return false;
     }
     public static function DoesItReformulateStatisticsProviderData(StatisticsProviderDecorator $statisticsProvider ) : bool
     {
         return $statisticsProvider instanceof ReformulatesStatisticsProviderData;
     }
-    protected function orderReformatorSatisticsProvider(StatisticsProviderDecorator $statisticsProvider) : void
+    protected function orderReformatorSatisticsProvider(StatisticsProviderDecorator $statisticsProvider) : bool
     {
         if($this::DoesItReformulateStatisticsProviderData($statisticsProvider))
         {
             $key = get_class($statisticsProvider);
             $this->ReformatorStatisticsProviders[ $key ] = $statisticsProvider;
+            return true;
         }
+        return false;
+    }
+    protected function orderStatisticsProvider(StatisticsProviderDecorator $statisticsProvider) : void
+    {
+        if ( $this->orderReformatorSatisticsProvider($statisticsProvider) ) { return; }
+        if( $this->orderReformulatableSatisticsProvider($statisticsProvider) ) {return ;}
+
+        /**
+         * The default behavior
+         */
+        $this->orderNormalPrioritedStatisticsProvider($statisticsProvider);
     }
     protected function initProvider(string $statisticsProvidersClass = "") : ?StatisticsProviderDecorator
     {
@@ -80,15 +108,14 @@ class StatisticsProvidersCategorizer implements Arrayable
         {
             if($provider = $this->initProvider($statisticsProviderClass))
             {
-                $this->orderReformatorSatisticsProvider($provider);
-                $this->orderReformulatableSatisticsProvider($provider);
+                $this->orderStatisticsProvider($provider);
             }
         }
     }
 
     public function toArray()
     {
-        return array_merge($this->ReformatorStatisticsProviders , $this->ReforulatableStatisticsProviders);
+        return array_merge($this->normalPrioritedStatisticsProviders , $this->ReformatorStatisticsProviders , $this->ReforulatableStatisticsProviders);
     }
 
     /**
